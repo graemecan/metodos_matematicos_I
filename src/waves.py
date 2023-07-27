ac372d693fc1e82545a24769f02e848eb60689da
Graeme Candlish <graemecandlish@gmail.com>
2023-07-27 11:19:13 -0400

    Removed unwanted files

--- src/waves.py
+++
@@ -1,9 +0,0 @@
-# src/waves.py
-
-import numpy as np
-
-def wave(frequency, amplitude=1, phase=0):
-    def _wave(time):
-        return amplitude * np.sin(2 * np.pi * frequency * time + phase)
-
-    return _wave
